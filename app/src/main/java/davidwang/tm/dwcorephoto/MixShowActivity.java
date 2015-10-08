f0540a0af7b7b1ba4d98b0aa1630058819813bf9
DavidWang <232190315@qq.com>
2015-10-08 17:55:54 +0800

    添加头部下拉。

--- app/src/main/java/davidwang/tm/dwcorephoto/MixShowActivity.java
+++ app/src/main/java/davidwang/tm/dwcorephoto/MixShowActivity.java
@@ -1,16 +1,24 @@
 package davidwang.tm.dwcorephoto;
 
 import android.os.Bundle;
-import android.widget.ListView;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
-public class MixShowActivity extends BaseActivity {
+import davidwang.tm.view.PullToZoomListView;
 
-    private ListView mixlist;
+public class MixShowActivity extends BaseActivity implements AdapterView.OnItemClickListener{
+
+    private PullToZoomListView mixlist;
+    private String[] adapterData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mix_show);
+        findID();
         AddToolbar();
     }
 
@@ -18,7 +26,15 @@ public class MixShowActivity extends BaseActivity {
     @Override
     protected void findID() {
         super.findID();
-        mixlist = (ListView)findViewById(R.id.mixlist);
+        mixlist = (PullToZoomListView)findViewById(R.id.mixlist);
+        adapterData = new String[] { "Activity","Service","Content Provider","Intent","BroadcastReceiver","ADT","Sqlite3","HttpClient","DDMS","Android Studio","Fragment","Loader" };
+
+        mixlist.setAdapter(new ArrayAdapter<String>(MixShowActivity.this,
+                android.R.layout.simple_list_item_1, adapterData));
+
+        mixlist.getHeaderView().setImageResource(R.mipmap.mixheadimg);
+        mixlist.getHeaderView().setScaleType(ImageView.ScaleType.CENTER_CROP);
+        mixlist.setOnItemClickListener(this);
     }
 
     @Override
@@ -26,5 +42,8 @@ public class MixShowActivity extends BaseActivity {
         super.InData();
     }
 
-
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Log.e("1",position+"haha");
+    }
 }
